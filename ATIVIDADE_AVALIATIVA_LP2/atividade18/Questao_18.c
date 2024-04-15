c7ccce6aa0762712abe8ec7f92175a137fba7f2f
Antonio-HOS <ahosantos.cic@uesc.br>
2024-04-15 19:04:25 -0300

    mais coisas

---
+++ ATIVIDADE_AVALIATIVA_LP2/atividade18/Questao_18.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int quantidade(char str[], int inicio, int fim);
+
+int main(){
+    char str_1[100];
+
+    printf("Digite uma string: ");
+    gets(str_1);
+
+    int tamanho = strlen(str_1);
+
+    int quant = quantidade(str_1, 0, tamanho - 1);
+    printf("A quantidade eh: %d\n", quant);
+
+    return 0;
+}
+
+int quantidade(char str[], int inicio, int fim) {
+    int cont = 0;
+
+    if (inicio > fim){ //Caso base: se inicio > fim então para de contar.
+        return 0;
+    }
+
+    if (str[inicio] == 'A' || str[inicio] == 'a'){
+        cont = cont + 1;
+    }
+
+    return cont + quantidade(str, inicio + 1, fim);
+}
--- ATIVIDADE_AVALIATIVA_LP2/lista_01.h
+++ ATIVIDADE_AVALIATIVA_LP2/lista_01.h
@@ -18,6 +18,10 @@ void normaliza(double x_inout[]);
 double soma_recursiva(double vetor[], int tamanho);
 double calculo_media(double vetor[], int tamanho);
 void inversor(double lista[], int n);
+void inverter_string(char str[], int inicio, int fim);
+int verificar_palindromo(char str[], int inicio, int fim);
+int maior_valor(int vetor[], int tam, int indice);
+int quantidade(char str[], int inicio, int fim);
 
 
 
